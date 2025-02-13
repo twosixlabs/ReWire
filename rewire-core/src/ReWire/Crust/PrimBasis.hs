26983539bb4d1527cc9180088222a3e502dfad97
Chris Hathhorn <chris.hathhorn@twosixtech.com>
2025-02-12 20:07:41 -0600

    Add missing prim types to enable TC later phases.

--- rewire-core/src/ReWire/Crust/PrimBasis.hs
+++ rewire-core/src/ReWire/Crust/PrimBasis.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Safe #-}
-module ReWire.Crust.PrimBasis (addPrims) where
+module ReWire.Crust.PrimBasis (addPrims, primDatas) where
 
 import ReWire.Annotation (Annote (MsgAnnote))
 import ReWire.Crust.Syntax (Kind (..), Ty (..), DataCon (..), DataDefn (..), FreeProgram)
-import ReWire.Crust.Types (refTy, strTy, (|->), arr, kmonad)
+import ReWire.Crust.Types (refTy, strTy, (|->), arr, kmonad, pairTy)
 
 import Data.List (foldl')
 import Data.Text (Text)
@@ -14,24 +14,33 @@ import ReWire.Unbound (s2n)
 -- The "primitive basis" has some magical voodoo that cannot be expressed in
 -- the concrete syntax... so here we are.
 
+-- TODO: raise an error for mis-typed included primitives (don't silently drop them).
 addPrims :: FreeProgram -> FreeProgram
-addPrims (ts, syns, vs) = (ts <> primDatas, syns, vs)
+addPrims (ts, syns, vs) = (filter ((`notElem` (dataName <$> primDatas)) . dataName) ts <> primDatas, syns, vs)
 
 primDatas :: [DataDefn]
 primDatas = map mkData
-      [ ("->",       KStar `KFun` (KStar `KFun` KStar),                  [])
-      , ("ReacT",    KStar `KFun` (KStar `KFun` (kmonad `KFun` kmonad)), [])
-      , ("StateT",   KStar `KFun` (kmonad `KFun` kmonad),                [])
-      , ("Identity", kmonad,                                             [])
-      , ("Vec",      KNat `KFun` KStar `KFun` KStar,                     [])
-      , ("Finite",   KNat `KFun` KStar,                                  [])
+      [ ("()",       KStar,                                              [nullDataCon "()" "()"])
       , ("+",        KNat `KFun` KNat  `KFun` KNat,                      [])
-      , ("Ref",      KStar `KFun` KStar,                                 [refCtor])
-      , ("Integer",  KStar,                                              [])
-      , ("String",   KStar,                                              [])
+      , ("->",       KStar `KFun` (KStar `KFun` KStar),                  [])
+      , ("A_",       KStar,                                              [])
       , ("Bool",     KStar,                                              [nullDataCon "False" "Bool", nullDataCon "True" "Bool"])
+      , ("ExtDev",   KStar `KFun` (KStar `KFun` KStar),                  [])
+      , ("Finite",   KNat `KFun` KStar,                                  [])
+      , ("Identity", kmonad,                                             [])
+      , ("Integer",  KStar,                                              [])
+      , ("Proxy",    KNat `KFun` KStar,                                  [proxyCtor])
+      , ("PuRe",     KStar `KFun` (KStar `KFun` KStar),                  pureCtors)
+      , ("R_",       KStar,                                              [])
+      , ("ReacT",    KStar `KFun` (KStar `KFun` (kmonad `KFun` kmonad)), [])
+      , ("Ref",      KStar `KFun` KStar,                                 [refCtor])
+      , ("StateT",   KStar `KFun` (kmonad `KFun` kmonad),                [])
+      , ("String",   KStar,                                              [])
+      , ("Vec",      KNat `KFun` KStar `KFun` KStar,                     [])
       , ("[_]",      KStar `KFun` KStar,                                 [])
-      , ("()",       KStar,                                              [nullDataCon "()" "()"])
+      -- These are intended for internal typechecking use only.
+      , ("-",        KNat `KFun` KNat,                                   [])
+      , ("*",        KNat `KFun` KNat  `KFun` KNat,                      [])
       ] <> map mkTuple' [2..128]
 
 msg :: Text -> Annote
@@ -41,12 +50,46 @@ mkData :: (Text, Kind, [DataCon]) -> DataDefn
 mkData (n, k, cs) = DataDefn (msg $ "Prim: " <> n) (s2n n) k cs
 
 nullDataCon :: Text -> Text -> DataCon
-nullDataCon c t = DataCon (MsgAnnote $ "Prim: " <> c <> " data ctor") (s2n c) ([] |-> TyCon (MsgAnnote $ "Prim: " <> t <> " type ctor") (s2n t))
+nullDataCon c t = DataCon (MsgAnnote $ "Prim: " <> c <> " data constructor") (s2n c) ([] |-> TyCon (MsgAnnote $ "Prim: " <> t <> " type constructor") (s2n t))
+
+pureCtors :: [DataCon]
+pureCtors = [doneCtor, pauseCtor]
+      where doneCtor :: DataCon
+            doneCtor = DataCon an (s2n "Done") ([s2n "s", s2n "o"] |-> tup tA tvS `arr` tPure)
+
+            pauseCtor :: DataCon
+            pauseCtor = DataCon an (s2n "Pause") ([s2n "s", s2n "o"] |-> tup tvO (tup tR tvS) `arr` tPure)
+
+            tPure :: Ty
+            tPure = TyApp an (TyApp an (TyCon an $ s2n "PuRe") tvS) tvO
+
+            tup :: Ty -> Ty -> Ty
+            tup = pairTy an
+
+            tA :: Ty
+            tA = TyCon an $ s2n "A_"
+
+            tR :: Ty
+            tR = TyCon an $ s2n "R_"
+
+            tvS :: Ty
+            tvS = TyVar an KStar $ s2n "s"
+
+            tvO :: Ty
+            tvO = TyVar an KStar $ s2n "o"
+
+            an :: Annote
+            an = MsgAnnote "Prim: PuRe data/type constructor"
+
+proxyCtor :: DataCon
+proxyCtor = DataCon an (s2n "Proxy") ([s2n "n"] |-> TyApp an (TyCon an $ s2n "Proxy") (TyVar an KNat $ s2n "n"))
+      where an :: Annote
+            an = MsgAnnote "Prim: Proxy data/type constructor"
 
 refCtor :: DataCon
 refCtor = DataCon an (s2n "Ref") $ [s2n "a"] |-> (strTy an `arr` refTy an (TyVar an KStar $ s2n "a"))
       where an :: Annote
-            an = MsgAnnote "Prim: Ref data/type ctor"
+            an = MsgAnnote "Prim: Ref data/type constructor"
 
 mkTuple' :: Int -> DataDefn
 mkTuple' n = DataDefn (msg "Prim: tuple") (s2n i) k [ctor]
